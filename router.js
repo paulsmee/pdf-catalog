bc516accb5b193a39941c5743d2da6470add9302
paulsmee <paulsmee.is@gmail.com>
2020-01-13 15:34:49 +1100

    first commit

---
+++ router.js
@@ -0,0 +1,61 @@
+'use strict'
+
+const express = require('express')
+const router = express.Router()
+const formidable = require('formidable')
+const mv = require('mv')
+const gm = require('gm');
+const fs = require('fs');
+const parse = require('node-html-parser').parse;
+
+
+const pdf = {}
+
+router.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html')
+    console.log("Access Detected!")
+});
+
+router.post('/fileupload', function (req, res) {
+    console.log('Attempting FileUpload')
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+        var oldpath = files.filetoupload.path;
+        var newpath = './public/pdf/' + files.filetoupload.name;
+        pdf.pdfstats = newpath
+        pdf.pdfname = files.filetoupload.name
+        getImage()
+        mv(oldpath, newpath, function (err) {
+            if (err) throw err;
+            res.redirect('/');
+            res.end();
+        });
+    })
+})
+
+function getImage() {
+    console.log('Working')
+    gm(pdf.pdfstats)
+        // var 
+        .write(pdf.pdfname + '.png', function (err) {
+            if (err) console.log('aaw, shucks' + err);
+            console.log('this worked!')
+        });
+}
+
+
+fs.readFile('index.html', 'utf8', (err, html) => {
+    if (err) {
+        throw err;
+    }
+
+    const root = parse(html);
+
+    const body = root.querySelector('body');
+    body.set_content('<div id = "asdf"></div>');
+    body.appendChild('<div id="uploadPDF">This Works?</div>');
+
+    console.log(root.toString()); // This you can write back to file!
+});
+
+module.exports = router
---
+++ web-server.js
@@ -0,0 +1,21 @@
+// This is the base server which also routes functions to the user.
+
+// 'use-strict' will ensure the file will fail if there are any errors,
+// it's a quick way to ensure your code is correct (though it still might not do what you expected).
+'use strict'
+
+// This loads the 'express' package so we can use the functions within it.
+var express = require('express')
+var server = express()
+
+server.use(express.static('public'))
+// Connect our router function from router.js (the module we exported at the bottom).
+server.use(require('./router'))
+
+// Specify which port to listen on
+var port = 8080
+//This is the action our server will take once it is ready to serve.
+server.listen(port, function () {
+    // This message is displayed on the host machine when the server is started.
+    console.log('The server is listening on port ' + port)
+})
